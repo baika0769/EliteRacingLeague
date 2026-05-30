66f1a84b48802f979b974e9f5f5257fb7f8fde36
BuiThanhKhai <baika0769@gmail.com>
2026-05-30 09:34:22 +0700

    update constants

---
+++ Constants/UserStatuses.cs
@@ -0,0 +1,21 @@
+﻿namespace Eliteracingleague.API.Constants;
+
+public static class UserStatuses
+{
+    public const string Active = "Active";
+    public const string Inactive = "Inactive";
+    public const string Banned = "Banned";
+
+    public static readonly string[] All =
+    {
+        Active,
+        Inactive,
+        Banned
+    };
+
+    public static bool IsValid(string? status)
+    {
+        return !string.IsNullOrWhiteSpace(status)
+            && All.Contains(status);
+    }
+}
